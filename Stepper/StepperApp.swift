3c13cbc5930b8ac60022fd5c6335874cb6a01a49
boris <barys.serzhanovich@gmail.com>
2026-04-21 01:16:21 +0300

    Initial Commit

---
+++ Stepper/StepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperApp.swift
+//  Stepper
+//
+//  Created by Boris Serzhanovich on 21.04.26.
+//
+
+import SwiftUI
+
+@main
+struct StepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
